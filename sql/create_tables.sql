da54f9f34fd26e18b12ba83a73b405e82cacd163
Anneli  Zapata <annelizaro@gmail.com>
2026-02-12 15:07:29 -0300

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,30 @@
+CREATE DATABASE taylor_swift_streams;
+USE taylor_swift_streams;
+
+-- Tabla albums
+CREATE TABLE albums (
+    album_id INT PRIMARY KEY AUTO_INCREMENT,
+    album_name VARCHAR(100),
+    release_year INT
+);
+
+-- Tabla songs
+CREATE TABLE songs (
+    song_id INT PRIMARY KEY,
+    name VARCHAR(150),
+    album_id INT,
+    release_year INT,
+    is_single BOOLEAN,
+    feature VARCHAR(150),
+    FOREIGN KEY (album_id) REFERENCES albums(album_id)
+);
+
+-- Tabla streams (la más importante)
+CREATE TABLE streams (
+    stream_id INT PRIMARY KEY AUTO_INCREMENT,
+    song_id INT,
+    stream_date DATE,
+    stream_count BIGINT,
+    FOREIGN KEY (song_id) REFERENCES songs(song_id)
+);
 
